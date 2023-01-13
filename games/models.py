8dc2547b7ab797e92b35bc9bff82c4b01fb4d7ff
SevaKuzn <luckeplay845@gmail.com>
2023-01-13 20:55:46 +0300

    styles

--- games/models.py
+++ games/models.py
@@ -9,8 +9,10 @@ class Game(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+    image = models.ImageField(blank=True)
+
     def __str__(self):
         return f'{self.title}'
+
 
 # Create your models here.
